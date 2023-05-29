5bafc624707ddd13f6a2fb806b4f31389b634a1b
andrea-w-wang <ayw255@nyu.edu>
2023-05-29 14:21:17 -0400

    first commit

---
+++ scripts/identify_token_lang_by_cld3.py
@@ -0,0 +1,18 @@
+import csv
+import numpy as np
+import pandas as pd
+import cld3
+model_family = 'mt5'
+vocab_file = f'vocab/{model_family}_vocab.npy'
+vocab = np.load(vocab_file, allow_pickle=True)
+map_file = f'vocab/m_x_u_{model_family}_map.npy'
+vocab_map = np.load(map_file, allow_pickle=True)
+subset_vocab = vocab[vocab_map]
+def preprocess(vocab):
+    vocab = vocab.lower()
+    vocab = vocab.strip("▁")
+    return vocab
+print("Token\tLangID\tConfidence\n")
+for vocab in subset_vocab:
+    r = cld3.get_language(preprocess(vocab))
+    print(f"{vocab}\t{r.language}\t{r['probability']:.3f}\n")
